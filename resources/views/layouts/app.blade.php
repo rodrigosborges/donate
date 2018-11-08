2ffd815e79d859d5d719142a62d41c5938e4377a
Rodrigo Borges <gmrodrigoborges@gmail.com>
2018-11-08 08:56:33 -0200

    adicionando icone

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,6 +24,10 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+    
+    <!-- Icon -->
+    <link rel="icon" type="image/png" href="asset('img/icones/donate.ico')">
+    
     @yield('style')
 </head>
 <body>
